64b5b5d35a35d9c92839c946e54891f0ac2f09a6
Webi Getachew <webigetachew26@gmail.com>
2025-04-07 23:38:43 +0300

    add header function

---
+++ calc.cpp
@@ -0,0 +1,36 @@
+#include <iostream> 
+using namespace std; 
+// Function prototypes 
+int add(int a, int b); 
+int subtract(int a, int b); 
+int multiply(int a, int b); 
+int divide(int a, int b); 
+ 
+int main() { 
+    int num1,num2; 
+    char operation; 
+ 
+    cout<<"Enter two numbers:"; 
+    cin>> num1>>num2; 
+ 
+    cout<<"Choose operation (+, -, *, /): "; 
+    cin>>operation; 
+ 
+    switch(operation) { 
+        case '+': 
+            cout<<"Result: " << add(num1, num2)<<endl; 
+            break; 
+        case '-': 
+            cout<<"Result: " << subtract(num1, num2)<<endl; 
+            break; 
+        case '*': 
+            cout<<"Result: "<<multiply(num1, num2)<<endl; 
+            break; 
+        case '/': 
+            cout<<"Result:"<<divide(num1, num2)<<endl; 
+            break; 
+        default: 
+            cout<<"Invalid operation!"<<endl; 
+    } 
+ 
+    return 0;
