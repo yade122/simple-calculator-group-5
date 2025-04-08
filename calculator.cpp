4d2808a8f671b28412384c60fd124dd1d2c27c64
yade122 <yadetamiru@gmail.com>
2025-04-07 16:44:14 -0800

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,16 @@
+#simple-calculator-group-5
+## this project the project calculator of c++ by computer science student group 2
+- DILLA UNIVERSIRY
+- COLLEGE OF ENGINEERNG AND TECHNOLOGY
+- COMPUTER SCIENCE STUDENT group 2
+## student name of the group
+  -webi getachow " implemented header file and main function"
+-tizbit alamayeho "implemented addition "
+-tolasa zewude "implemented multiplication"
+-yohanis tofik "iplemented subsatraction
+yadetan tamiru "implemented division and readme"
+## How to Run the Program
+git clone yade122/simple-calculator-group-5
+
+  
+
